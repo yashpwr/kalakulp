624faf509d824b88cea90c88074c7b97d728ba93
yash <yashdvn@gmail.com>
2020-03-31 14:56:53 +0530

    removed category it seems like unused

--- resources/views/backend/include/sidebar.blade.php
+++ resources/views/backend/include/sidebar.blade.php
@@ -27,17 +27,6 @@ $currRoute = Route::current()->getName();
                     </ul>
                 </li>
 
-                <li class="{{ $currRoute == 'categorylist' || $currRoute == 'addcategory' || $currRoute == 'updatecategory'  ? 'active mm-active' : '' }}">
-                    <a href="javascript: void(0);" class="has-arrow waves-effect {{ $currRoute == 'categorylist' || $currRoute == 'addcategory' || $currRoute == 'updatecategory'  ? 'active mm-active' : '' }}">
-                        <i class="bx bx-layout"></i>
-                        <span>Category</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{route('categorylist')}}">Category List</a></li>
-                        <li><a href="{{route('addcategory')}}">Add Category</a></li>
-                    </ul>
-                </li>
-
                 <li class="{{ $currRoute == 'FabricList' || $currRoute == 'addfabric' || $currRoute == 'updateFabric'  ? 'active mm-active' : '' }}">
                     <a href="javascript: void(0);" class="has-arrow waves-effect {{ $currRoute == 'FabricList' || $currRoute == 'addfabric' || $currRoute == 'updateFabric'  ? 'active mm-active' : '' }}">
                         <i class="bx bx-layout"></i>
